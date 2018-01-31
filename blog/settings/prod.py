fbddfa531d34db5414290aee96611a2d78186475
Jack Evans <jack@evans.gb.net>
2018-01-31 11:52:03 +0000

    Update prod.py settings to load settings from environment variables

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,25 +1,37 @@
 from .base import *
 import os
 
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name):
+    """Get the environment variable or return exception"""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f"Set the {var_name} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+
 # Load the secret key from environment variable
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # Disable debugging
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 # Load server IP from environment variable
-SERVER_IP = os.environ['SERVER_IP']
+DOMAIN_NAME = get_env_variable('DOMAIN_NAME')
+SERVER_IP = get_env_variable('SERVER_IP')
 ALLOWED_HOSTS = [SERVER_IP]
 
-STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_ROOT = 'static'
 
-SECURE_HSTS_SECONDS = 3600
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# X_FRAME_OPTIONS = 'DENY'
+# SECURE_HSTS_PRELOAD = True
