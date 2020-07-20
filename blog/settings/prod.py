99f45be50bd1158ecb4e21ab2e759d8fce625a0d
Jack Evans <jack@evans.gb.net>
2020-07-20 20:25:12 +0100

    initial commit

---
+++ blog/settings/prod.py
@@ -0,0 +1,39 @@
+from .base import *
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """Get the environment variable or return exception"""
+    try:
+        return os.environ[var_name]
+
+    except KeyError:
+        error_msg = f"Set the {var_name} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+
+# Load the secret key from environment variable
+SECRET_KEY = get_env_variable("SECRET_KEY")
+SERVER_IP = get_env_variable("SERVER_IP")
+DOMAIN_NAME = get_env_variable("DOMAIN_NAME")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+
+# Load server IP from environment variable
+ALLOWED_HOSTS = [SERVER_IP, DOMAIN_NAME]
+
+# Disable debugging
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECURE_HSTS_SECONDS = 31_536_000
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_HSTS_PRELOAD = True
---
+++ posts/models.py
@@ -0,0 +1,73 @@
+from markdown import markdown as md
+
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.text import slugify
+from django.utils.timezone import now
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    color_code = models.CharField(max_length=7)
+
+    class Meta:
+        ordering = ("name",)
+
+    def colored_code(self):
+        return format_html('<span style="color: {0};"> {0}</span>', self.color_code)
+
+    def get_absolute_url(self):
+        return reverse("posts:tag_detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class DraftsPostManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_draft=True)
+
+
+class PublishedPostManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_draft=False)
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    pub_date = models.DateTimeField(default=now)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    content = models.TextField()
+    markdown = models.TextField(blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_draft = models.BooleanField(default=True)
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedPostManager()
+    drafts = DraftsPostManager()
+
+    class Meta:
+        ordering = ("-pub_date",)
+
+    def get_absolute_url(self):
+        return reverse("posts:post_detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.markdown = md(
+            self.content,
+            extensions=["fenced_code", "codehilite"],
+        )
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
