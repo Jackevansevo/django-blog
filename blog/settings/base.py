292fdcf89426a4bf6c7b813252f2a4143effa358
Jack Evans <jack@evans.gb.net>
2021-04-09 15:22:49 +0100

    update django version

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -3,6 +3,8 @@ import os
 LOGIN_REDIRECT_URL = "posts:index"
 LOGOUT_REDIRECT_URL = "posts:index"
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
